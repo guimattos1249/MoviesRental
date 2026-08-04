487a0f9237325f243ef06560e38fa1b81d662c37
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-04 17:27:02 -0300

    feat: creating entity base

--- src/BuildingBlocks/MoviesRental.Core/DomainObjects/Entity.cs
+++ src/BuildingBlocks/MoviesRental.Core/DomainObjects/Entity.cs
@@ -1,5 +1,14 @@
 ﻿namespace MoviesRental.Core.DomainObjects;
 
-internal class Entity
+public class Entity
 {
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.Now;
+    }
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime UpdatedAt { get; protected set; }
+    public DateTime? DeletedAt { get; protected set; }
 }
