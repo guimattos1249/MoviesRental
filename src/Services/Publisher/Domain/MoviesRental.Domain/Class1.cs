f2cf9d4c9592bd8d5b1c01f721a1c0f0ab901183
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-04 17:41:42 -0300

    feat: add director

--- src/Services/Publisher/Domain/MoviesRental.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MoviesRental.Domain;
-
-public class Class1
-{
-
-}
---
+++ src/Services/Publisher/Domain/MoviesRental.Domain/Entities/Director.cs
@@ -0,0 +1,45 @@
+﻿using MoviesRental.Core.DomainObjects;
+using System.Text.RegularExpressions;
+
+namespace MoviesRental.Domain.Entities;
+
+public class Director
+{
+    public Director(string name, string surname)
+    {
+        UpdateName(name);
+        UpdateSurname(surname);
+    }
+
+    const int MaxNameLength = 100;
+    const int MinNameLength = 3;
+
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    private List<Dvd> _dvds = [];
+    public IReadOnlyList<Dvd> Dvds => _dvds.AsReadOnly();
+
+    public string FullName => $"{Name} {Surname}";
+
+    public void UpdateName(string name)
+    {
+        if (ValidateName(name))
+            throw new DomainException("Director name cannot be empty.");
+        Name = name;
+    }
+
+    public void UpdateSurname(string surname)
+    {
+        if (ValidateName(surname))
+            throw new DomainException("Director surname cannot be empty.");
+        Surname = surname;
+    }
+
+    private static bool ValidateName(string name)
+    {
+        if (!string.IsNullOrWhiteSpace(name) || name.Length < MinNameLength || name.Length > MaxNameLength)
+            return false;
+
+        return Regex.IsMatch(name, "^(?=.*[A-ZÀ-ÿ~])(?=.*[a-zà-ÿ~])[A-Za-zÀ-ÿ~]+$");
+    }
+}
