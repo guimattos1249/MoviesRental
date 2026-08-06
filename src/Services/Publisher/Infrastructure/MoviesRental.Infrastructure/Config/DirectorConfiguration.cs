564fcab180276d5b5d64da4c42d233cddb2f9d4f
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-06 19:07:09 -0300

    feat: add EF configuration

---
+++ src/Services/Publisher/Infrastructure/MoviesRental.Infrastructure/Config/DirectorConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoviesRental.Domain.Entities;
+
+namespace MoviesRental.Infrastructure.Config;
+
+public class DirectorConfiguration : IEntityTypeConfiguration<Director>
+{
+    public void Configure(EntityTypeBuilder<Director> builder)
+    {
+        builder.ToTable("Directors");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(Director.MaxNameLength);
+
+        builder.Property(x => x.Surname)
+            .IsRequired()
+            .HasMaxLength(Director.MaxNameLength);
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.Property(x => x.UpdatedAt)
+            .IsRequired();
+
+        builder.HasMany(x => x.Dvds)
+            .WithOne(d => d.Director);
+    }
+}
