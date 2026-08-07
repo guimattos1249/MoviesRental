40c94559c15f3e44a86c5c6cf157b34bff102211
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-07 12:50:51 -0300

    feat: add api base response

---
+++ src/BuildingBlocks/MoviesRental.Core/BaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesRental.Core;
+
+public class BaseResponse
+{
+    public int StatusCode { get; set; }
+    public bool Success { get; set; }
+    public string? Message { get; set; }
+    public object? Data { get; set; }
+}
