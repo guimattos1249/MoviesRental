49c1b99260e09f361332d370a9ff67c6ad362400
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-07 13:17:28 -0300

    feat: add use case Createdirector commands
    This use case will create a director on read database

---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Directors/Commands/CreateDirector/CreateDirectorCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace MoviesRental.Queries.Application.UseCases.Directors.Commands.CreateDirector;
+
+public record CreateDirectorCommand(string Id, string FullName, DateTime CreatedAt, DateTime UpdatedAt) : IRequest<bool>;
---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Directors/Commands/CreateDirector/CreateDirectorCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using MoviesRental.Queries.Application.Contracts;
+using MoviesRental.Queries.Domain.Models;
+
+namespace MoviesRental.Queries.Application.UseCases.Directors.Commands.CreateDirector;
+
+public class CreateDirectorCommandHandler(IDirectorsQueryRepository repository, CreateDirectorCommandValidator validator) : IRequestHandler<CreateDirectorCommand, bool>
+{
+    private readonly IDirectorsQueryRepository _repository = repository;
+    private readonly CreateDirectorCommandValidator _validator = validator;
+
+    public async Task<bool> Handle(CreateDirectorCommand request, CancellationToken cancellationToken)
+    {
+        var validationResult = _validator.Validate(request);
+        if (!validationResult.IsValid)
+            return default;
+
+        var director = await _repository.GetAsync(request.Id);
+        if (director is not null)
+            return false;
+
+        director = new Director { Id = request.Id, FullName = request.FullName, CreatedAt = request.CreatedAt, UpdatedAt = request.UpdatedAt };
+        var result = await _repository.CreateAsync(director);
+        if (result is null)
+            return default;
+
+        return true;
+    }
+}
