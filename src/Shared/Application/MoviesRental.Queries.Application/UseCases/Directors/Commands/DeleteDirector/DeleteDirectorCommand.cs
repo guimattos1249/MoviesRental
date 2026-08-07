69ce55135206880f3a19399d8b0f7b5caf51be19
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-07 13:23:47 -0300

    feat: add use case delete director commands
    This use case will create a director on read database

---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Directors/Commands/DeleteDirector/DeleteDirectorCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace MoviesRental.Queries.Application.UseCases.Directors.Commands.DeleteDirector;
+
+public record DeleteDirectorCommand(string Id) : IRequest<bool>;
---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Directors/Commands/DeleteDirector/DeleteDirectorCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using MoviesRental.Queries.Application.Contracts;
+
+namespace MoviesRental.Queries.Application.UseCases.Directors.Commands.DeleteDirector;
+
+public class DeleteDirectorCommandHandler(IDirectorsQueryRepository repository) : IRequestHandler<DeleteDirectorCommand, bool>
+{
+    private readonly IDirectorsQueryRepository _repository = repository;
+
+    public async Task<bool> Handle(DeleteDirectorCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.Id))
+            return false;
+
+        var director = await _repository.GetAsync(request.Id);
+        if (director is null)
+            return false;
+
+        return await _repository.DeleteAsync(director.Id);
+    }
+}
