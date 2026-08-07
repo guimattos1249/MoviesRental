f1c6f4f905e079ac04761d1a34be2f322deb7525
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-07 12:51:05 -0300

    feat: add director controller

---
+++ src/Services/Publisher/API/MoviesRental.API/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using MoviesRental.Core;
+
+namespace MoviesRental.API.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class ApiController : ControllerBase
+{
+    protected IActionResult CustomResponse(int status, bool success, object? data = null)
+    {
+        return (status, success) switch
+        {
+            (404, false) => NotFound(new BaseResponse { StatusCode = status, Success = success, Message = "no elements found" }),
+            (400, false) => BadRequest(new BaseResponse { StatusCode = status, Success = success, Message = "bad request" }),
+            (500, false) => StatusCode(500, new BaseResponse { StatusCode = status, Success = success, Message = "internal server error" }),
+            (201, true) => Created(string.Empty, new BaseResponse { StatusCode = status, Success = success, Message = "resource created successfully", Data = data }),
+            (200, true) => Ok(new BaseResponse { StatusCode = status, Success = success, Data = data }),
+            _ => StatusCode(500, new BaseResponse { StatusCode = status, Success = success, Message = "internal server error" })
+        };
+    }
+}
---
+++ src/Services/Publisher/API/MoviesRental.API/Controllers/DirectorController.cs
@@ -0,0 +1,69 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MoviesRental.Application.UseCases.Directors.Commands.CreateDirector;
+using MoviesRental.Application.UseCases.Directors.Commands.DeleteDirector;
+using MoviesRental.Core;
+using System.Net;
+
+namespace MoviesRental.API.Controllers;
+
+public class DirectorController(IMediator mediator) : ApiController
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpGet("[action]/{fullName}", Name = "GetDirector")]
+    [ProducesResponseType(typeof(BaseResponse), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public async Task<IActionResult> GetDirector([FromRoute] string fullName)
+    {
+        var query = new GetDirectorQuery(fullName);
+        var response = await _mediator.Send(query, HttpContext.RequestAborted);
+
+        if (response is null)
+            return CustomResponse((int)HttpStatusCode.NotFound, false, null);
+
+        return CustomResponse((int)HttpStatusCode.OK, true, response);
+    }
+
+    [HttpPost("CreateDirector")]
+    [ProducesResponseType(typeof(BaseResponse), (int)HttpStatusCode.Created)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> CreateDirector(
+        [FromBody] CreateDirectorCommand command)
+    {
+        var response = await _mediator.Send(command, HttpContext.RequestAborted);
+
+        if (response is null)
+            return CustomResponse((int)HttpStatusCode.BadRequest, false, null);
+
+        return CustomResponse((int)HttpStatusCode.Created, true, response);
+    }
+
+    [HttpPost("UpdateDirector")]
+    [ProducesResponseType(typeof(BaseResponse), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> UpdateDirector(
+        [FromBody] CreateDirectorCommand command)
+    {
+        var response = await _mediator.Send(command, HttpContext.RequestAborted);
+
+        if (response is null)
+            return CustomResponse((int)HttpStatusCode.BadRequest, false, null);
+
+        return CustomResponse((int)HttpStatusCode.OK, true, response);
+    }
+
+    [HttpPost("DeleteDirector")]
+    [ProducesResponseType(typeof(BaseResponse), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> DeleteDirector([FromRoute] Guid id)
+    {
+        var command = new DeleteDirectorCommand(id);
+        var response = await _mediator.Send(command, HttpContext.RequestAborted);
+
+        if (!response)
+            return CustomResponse((int)HttpStatusCode.BadRequest, response);
+
+        return CustomResponse((int)HttpStatusCode.OK, response);
+    }
+}
