049188286bccf7f9732e79fdbb2d889d0ca30b37
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-08 10:28:06 -0300

    feat: add return dvd use case to query

---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Dvds/Commands/ReturnDvd/ReturnDvdCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace MoviesRental.Queries.Application.UseCases.Dvds.Commands.ReturnDvd;
+
+public record ReturnDvdCommand(string Id, DateTime UpdatedAt) : IRequest<bool>;
---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Dvds/Commands/ReturnDvd/ReturnDvdCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using MoviesRental.Queries.Application.Contracts;
+
+namespace MoviesRental.Queries.Application.UseCases.Dvds.Commands.ReturnDvd;
+
+public class ReturnDvdCommandHandler(IDvdsQueryRepository repository) : IRequestHandler<ReturnDvdCommand, bool>
+{
+    private readonly IDvdsQueryRepository _repository = repository;
+
+    public async Task<bool> Handle(ReturnDvdCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.Id) || request.UpdatedAt > DateTime.UtcNow)
+            return false;
+
+        var dvd = await _repository.GetAsync(request.Id);
+        if (dvd is null)
+            return false;
+
+        dvd.Copies += 1;
+
+        return await _repository.UpdateAsync(dvd);
+    }
+}
