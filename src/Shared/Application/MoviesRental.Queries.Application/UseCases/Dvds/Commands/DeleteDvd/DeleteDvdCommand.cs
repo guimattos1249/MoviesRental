90243bb48f37f62865109e9bdd15e3d3da784c7a
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-08 10:19:41 -0300

    feat: add update dvd use case to query

---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Dvds/Commands/DeleteDvd/DeleteDvdCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace MoviesRental.Queries.Application.UseCases.Dvds.Commands.DeleteDvd;
+
+public record DeleteDvdCommand(string Id, DateTime DeletedAt) : IRequest<bool>;
---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Dvds/Commands/DeleteDvd/DeleteDvdHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using MoviesRental.Queries.Application.Contracts;
+using MoviesRental.Queries.Application.UseCases.Dvds.Commands.DeleteDvd;
+
+namespace MoviesRental.Application.UseCases.Dvds.Commands.DeleteDvd;
+
+public class DeleteDvdHandler(IDvdsQueryRepository repository) : IRequestHandler<DeleteDvdCommand, bool>
+{
+    private readonly IDvdsQueryRepository _repository = repository;
+
+    public async Task<bool> Handle(DeleteDvdCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.Id) || request.DeletedAt > DateTime.UtcNow)
+            return false;
+
+        var dvd = await _repository.GetAsync(request.Id);
+        if (dvd is null)
+            return false;
+
+        dvd.Available = false;
+        dvd.DeletedAt = request.DeletedAt;
+        dvd.Copies = 0;
+
+        return await _repository.UpdateAsync(dvd);
+    }
+}
