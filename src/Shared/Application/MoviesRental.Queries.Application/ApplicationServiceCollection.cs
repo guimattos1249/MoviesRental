99641b960c2c8131c93696a9ed0fe94e765a86f6
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-08 12:11:23 -0300

    refactor: changed name of dependency injection method

---
+++ src/Shared/Application/MoviesRental.Queries.Application/ApplicationServiceCollection.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MoviesRental.Queries.Application;
+
+public static class ApplicationServiceCollection
+{
+    public static IServiceCollection AddReadApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly(), ServiceLifetime.Scoped);
+        services.AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
