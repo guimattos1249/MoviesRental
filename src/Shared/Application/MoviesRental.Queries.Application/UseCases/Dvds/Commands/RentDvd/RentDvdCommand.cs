9516240742d2e8602bcb7a0a5eb87446516f85ed
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-08 10:24:41 -0300

    feat: add rent dvd use case to query

---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Dvds/Commands/RentDvd/RentDvdCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace MoviesRental.Queries.Application.UseCases.Dvds.Commands.RentDvd;
+
+public record RentDvdCommand(string Id, DateTime UpdatedAt) : IRequest<bool>;
---
+++ src/Shared/Application/MoviesRental.Queries.Application/UseCases/Dvds/Commands/RentDvd/RentDvdCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using MoviesRental.Queries.Application.Contracts;
+
+namespace MoviesRental.Queries.Application.UseCases.Dvds.Commands.RentDvd;
+
+public class RentDvdCommandHandler(IDvdsQueryRepository repository) : IRequestHandler<RentDvdCommand, bool>
+{
+    private readonly IDvdsQueryRepository _repository = repository;
+
+    public async Task<bool> Handle(RentDvdCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.Id) || request.UpdatedAt > DateTime.UtcNow)
+            return false;
+
+        var dvd = await _repository.GetAsync(request.Id);
+        if (dvd is null || dvd is { Copies: 0 })
+            return false;
+
+        dvd.Copies -= 1;
+
+        return await _repository.UpdateAsync(dvd);
+    }
+}
