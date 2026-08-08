633e455fb5053beb3621ffdeffd9d662d1517580
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-08 12:36:09 -0300

    feat: add dvd mongodb repository

---
+++ src/Shared/Infrastructure/MoviesRental.Queries.Infrastructure/InfrastructureServiceCollection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MoviesRental.Queries.Application.Contracts;
+using MoviesRental.Queries.Infrastructure.Context;
+using MoviesRental.Queries.Infrastructure.Repositories;
+
+namespace MoviesRental.Queries.Infrastructure;
+
+public static class InfrastructureServiceCollection
+{
+    public static IServiceCollection AddReadInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IMoviesRentalReadContext, MoviesRentalReadContext>();
+        services.AddScoped<IDirectorsQueryRepository, DirectorsQueryRepository>();
+        services.AddScoped<IDvdsQueryRepository, DvdsQueryRepository>();
+
+        return services;
+    }
+}
---
+++ src/Shared/Infrastructure/MoviesRental.Queries.Infrastructure/Repositories/DvdsQueryRepository.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using MoviesRental.Queries.Application.Contracts;
+using MoviesRental.Queries.Domain.Models;
+using MoviesRental.Queries.Infrastructure.Context;
+using System.Xml.Linq;
+
+namespace MoviesRental.Queries.Infrastructure.Repositories;
+
+public class DvdsQueryRepository(IMoviesRentalReadContext context) : IDvdsQueryRepository
+{
+    private readonly IMoviesRentalReadContext _context = context;
+
+    public async Task<Dvd> CreateAsync(Dvd entity)
+    {
+        await _context.Dvds.InsertOneAsync(entity);
+        return entity;
+    }
+
+    public async Task<bool> DeleteAsync(string Id)
+    {
+        var result = await _context.Dvds.DeleteOneAsync(d => d.Id == Id);
+        return result.IsAcknowledged && result.DeletedCount > 0;
+    }
+
+    public async Task<Dvd> GetAsync(string Id) =>
+        await _context
+            .Dvds
+            .Find(d => d.Id == Id)
+            .FirstOrDefaultAsync();
+
+    public async Task<Dvd> GetByTitleAsync(string title) =>
+        await _context
+            .Dvds
+            .Find(d => d.Title == title && d.Available)
+            .FirstOrDefaultAsync();
+
+    public async Task<bool> UpdateAsync(Dvd entity)
+    {
+        var result = await _context
+            .Dvds
+            .ReplaceOneAsync(d => d.Id == entity.Id, entity);
+
+        return result.IsAcknowledged && result.ModifiedCount > 0;
+    }
+}
