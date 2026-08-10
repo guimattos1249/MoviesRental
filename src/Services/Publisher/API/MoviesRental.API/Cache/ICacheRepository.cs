12eaebbf939b087386fe0242c6d4b6917a292179
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-10 12:43:26 -0300

    feat: add cache repository

---
+++ src/Services/Publisher/API/MoviesRental.API/Cache/ICacheRepository.cs
@@ -0,0 +1,9 @@
+﻿using MoviesRental.Queries.Application.UseCases.Dvds.Queries.GetDvd;
+
+namespace MoviesRental.API.Cache;
+
+public interface ICacheRepository
+{
+    Task<GetDvdResponse> GetAsync(string title);
+    Task Update(GetDvdResponse response);
+}
