347c2c92aa346f03271d2d96085ef15290d086f7
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-10 12:44:15 -0300

    refactor: refactoring setup configs

---
+++ src/Services/Publisher/API/MoviesRental.API/Setup/ApiConfig.cs
@@ -0,0 +1,60 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MoviesRental.Infrastructure.Context;
+using MoviesRental.Queries.Infrastructure.Settings;
+
+namespace MoviesRental.API.Setup;
+
+public static class ApiConfig
+{
+    public static IServiceCollection AddApiConfig(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDependencyInjection();
+        services.AddDbContext<MoviesRentalWriteContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlConnection"), opt =>
+            {
+                opt.EnableRetryOnFailure();
+            });
+        });
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+        });
+
+        services.AddMassTransit(config =>
+        {
+            config.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(configuration["EventBusSettings:HostAddress"]);
+            });
+        });
+
+        var cacheSettings = configuration["CacheSettings:ConnectionString"]
+    ?? throw new InvalidOperationException(
+        "Settings string 'CacheSettings' was not configured.");
+        var sqlConnection = configuration.GetConnectionString("SqlConnection")
+            ?? throw new InvalidOperationException(
+                "Connection string 'SqlConnection' was not configured.");
+
+        services.Configure<MongoDbSettings>(configuration.GetSection(nameof(MongoDbSettings)));
+        services.AddSingleton(sp => sp.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+
+        services.AddApiVersioning();
+        services.AddHealthChecks()
+            .AddRedis(cacheSettings, "Cache HealthCheck", HealthStatus.Degraded)
+            .AddMongoDb(
+                sp => sp.GetRequiredService<IMongoClient>(),
+                sp => sp.GetRequiredService<MongoDbSettings>().DatabaseName,
+                name: "MongoDB",
+                failureStatus: HealthStatus.Degraded)
+            .AddSqlServer(sqlConnection);
+
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+
+        return services;
+    }
+}
---
+++ src/Services/Publisher/API/MoviesRental.API/Setup/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using MoviesRental.API.Cache;
+using MoviesRental.Application;
+using MoviesRental.Infrastructure;
+using MoviesRental.Queries.Application;
+using MoviesRental.Queries.Infrastructure;
+
+namespace MoviesRental.API.Setup;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+    {
+        services.AddWriteApplication();
+        services.AddWriteInfrastructure();
+        services.AddReadApplication();
+        services.AddReadInfrastructure();
+        services.AddScoped<ICacheRepository, CacheRepository>();
+
+        return services;
+    }
+}
