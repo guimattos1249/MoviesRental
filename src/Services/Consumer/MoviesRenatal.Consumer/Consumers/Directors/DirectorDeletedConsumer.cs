7d99fdb8f4bc3d3c0d25abe6c2e4b3320e1edbd1
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-11 12:32:03 -0300

    feat: add deleted director consumer

---
+++ src/Services/Consumer/MoviesRenatal.Consumer/Consumers/Directors/DirectorDeletedConsumer.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using MediatR;
+using MongoDB.Driver;
+using MoviesRental.Core.EventBus.Events;
+using MoviesRental.Queries.Application.UseCases.Directors.Commands.DeleteDirector;
+
+namespace MoviesRenatal.Consumer.Consumers.Directors;
+
+public class DirectorDeletedConsumer(IMediator mediator, ILogger<DirectorCreatedConsumer> logger) : IConsumer<DirectorDeletedEvent>
+{
+    private readonly IMediator _mediator = mediator;
+    private ILogger<DirectorCreatedConsumer> _logger = logger;
+
+    public async Task Consume(ConsumeContext<DirectorDeletedEvent> context)
+    {
+        try
+        {
+            var @event = context?.Message ?? throw new ArgumentException(null, nameof(context));
+            var command = new DeleteDirectorCommand(@event.Id);
+            _logger.LogInformation($"Deleting director {command.Id}");
+
+            var response = await _mediator.Send(command, default);
+
+            if (!response)
+            {
+                _logger.LogError($"Somenthing wrong happende during the deletion of director {@event.Id}");
+                throw new InvalidOperationException($"Falied to delete director {@event.Id}");
+            }
+            _logger.LogInformation($"Director {@event.Id} deleted");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error ocurred while consuming the DirectorDeletedEvent");
+            throw;
+        }
+    }
+}
