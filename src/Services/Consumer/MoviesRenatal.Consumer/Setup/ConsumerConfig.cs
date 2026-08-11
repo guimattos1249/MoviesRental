511c65a6384bde57943d46f7417245b7a1a9ced7
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-08-11 13:19:54 -0300

    feat: add masstransit config

--- src/Services/Consumer/MoviesRenatal.Consumer/Setup/ConsumerConfig.cs
+++ src/Services/Consumer/MoviesRenatal.Consumer/Setup/ConsumerConfig.cs
@@ -1,5 +1,8 @@
 ﻿using MassTransit;
 using Microsoft.Extensions.Options;
+using MoviesRenatal.Consumer.Consumers.Directors;
+using MoviesRenatal.Consumer.Consumers.Dvds;
+using MoviesRental.Core.EventBus;
 using MoviesRental.Queries.Application;
 using MoviesRental.Queries.Infrastructure;
 using MoviesRental.Queries.Infrastructure.Settings;
@@ -17,7 +20,50 @@ public static class ConsumerConfig
 
         services.AddMassTransit(config =>
         {
-
+            config.AddConsumer<DirectorCreatedConsumer>();
+            config.AddConsumer<DirectorUpdatedConsumer>();
+            config.AddConsumer<DirectorDeletedConsumer>();
+            config.AddConsumer<DvdCreatedConsumer>();
+            config.AddConsumer<DvdUpdatedConsumer>();
+            config.AddConsumer<DvdDeletedConsumer>();
+            config.AddConsumer<DvdRentedConsumer>();
+            config.AddConsumer<DvdReturnedConsumer>();
+            config.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(configuration["EventBusSettings:HostAddress"]);
+                cfg.ReceiveEndpoint(EventBusConstants.CREATED_DIRECTOR_QUEUE, c =>
+                {
+                    c.ConfigureConsumer<DirectorCreatedConsumer>(ctx);
+                });
+                cfg.ReceiveEndpoint(EventBusConstants.UPDATED_DIRECTOR_QUEUE, c =>
+                {
+                    c.ConfigureConsumer<DirectorUpdatedConsumer>(ctx);
+                });
+                cfg.ReceiveEndpoint(EventBusConstants.DELETED_DIRECTOR_QUEUE, c =>
+                {
+                    c.ConfigureConsumer<DirectorDeletedConsumer>(ctx);
+                });
+                cfg.ReceiveEndpoint(EventBusConstants.CREATED_DVD_QUEUE, c =>
+                {
+                    c.ConfigureConsumer<DvdCreatedConsumer>(ctx);
+                });
+                cfg.ReceiveEndpoint(EventBusConstants.UPDATED_DVD_QUEUE, c =>
+                {
+                    c.ConfigureConsumer<DvdUpdatedConsumer>(ctx);
+                });
+                cfg.ReceiveEndpoint(EventBusConstants.DELETED_DVD_QUEUE, c =>
+                {
+                    c.ConfigureConsumer<DvdDeletedConsumer>(ctx);
+                });
+                cfg.ReceiveEndpoint(EventBusConstants.RENTED_DVD_QUEUE, c =>
+                {
+                    c.ConfigureConsumer<DvdRentedConsumer>(ctx);
+                });
+                cfg.ReceiveEndpoint(EventBusConstants.RETURNED_DVD_QUEUE, c =>
+                {
+                    c.ConfigureConsumer<DvdReturnedConsumer>(ctx);
+                });
+            });
         });
 
         return services;
